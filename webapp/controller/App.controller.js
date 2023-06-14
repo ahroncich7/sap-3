867656abc3c74d17872bc1bbc223f6e7ddf4cf94
Alejandro Hroncich <alehroncich@gmail.com>
2023-06-14 19:21:45 +0000

    Initial Commit

---
+++ webapp/controller/App.controller.js
@@ -0,0 +1,14 @@
+sap.ui.define(
+    [
+        "sap/ui/core/mvc/Controller"
+    ],
+    function(BaseController) {
+      "use strict";
+  
+      return BaseController.extend("dest3.destination3.controller.App", {
+        onInit() {
+        }
+      });
+    }
+  );
+  
---
+++ webapp/test/unit/AllTests.js
@@ -0,0 +1,5 @@
+sap.ui.define([
+	"dest3/destination3/test/unit/controller/View1.controller"
+], function () {
+	"use strict";
+});
